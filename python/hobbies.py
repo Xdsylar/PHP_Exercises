a643d5f09356f8e9e9efb9547444e236e5fbf29c
Xdsylar <94957870+Xdsylar@users.noreply.github.com>
2022-07-05 10:55:17 +0200

    added one of my first python exercises
    the program first ask what your three hobbies are and then it tells you your three hobbies that you told the program

---
+++ python/hobbies.py
@@ -0,0 +1,6 @@
+hobby1 =input("Wat is je hobby?""\n")
+hobby2 =input("Wat is je hobby?""\n")
+hobby3 =input("Wat is je hobby?""\n")
+
+hobbies =["Je hobby's zijn",hobby1,hobby2,hobby3]
+print (hobbies)
