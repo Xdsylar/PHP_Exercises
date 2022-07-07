ab19dec52cceec36c25ed5400dd7cf31712d913a
Xdsylar <94957870+Xdsylar@users.noreply.github.com>
2022-07-07 10:33:43 +0200

    added a few of my first php exercises

---
+++ php/Lab1.03.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="nl">
+	<head>
+		<meta charset="uft-8">
+		<title> Lab1.03 </title>
+	</head>
+
+<body>
+	<?php
+		$Breedte = "10" ;
+		$Lengte = "11" ;
+		$Hoogte = "5";
+		$Oppervlakte = $Breedte * $Lengte ;
+		$Volume = $Breedte * $Lengte * $Hoogte ;
+
+		echo " De oppervlate van de container is = $Oppervlakte " ;
+		echo " <br> Het Volume van de container is = $Volume "
+	?>
+
+</body>
+
+</html>
---
+++ php/Lab1_02.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="nl">
+	<head>
+		<meta charset="uft-8">
+		<title> Lab1.02.php. </title>
+	</head>
+
+
+	<body>
+	<?php
+	echo "dit is mijn eerste PHP-Script. " ;
+	?>
+	</body>
+</html>
