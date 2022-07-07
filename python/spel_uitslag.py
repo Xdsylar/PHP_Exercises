fbe6c1fde2739f269b27d563fdb7e5624d68e9ae
Xdsylar <94957870+Xdsylar@users.noreply.github.com>
2022-07-07 09:50:15 +0200

    Added an python file
    In this python file the program asks you to give him the scores of each player, after that he will announce who the winner is or if the scores are equal: there is a tie

---
+++ python/spel_uitslag.py
@@ -0,0 +1,8 @@
+Scorespeler1 =int,input("Wat is de score van speler 1?""\n")
+Scorespeler2 =int,input("Wat is de score van speler 2?""\n")
+if Scorespeler1 > Scorespeler2:
+    print("Speler 1 heeft gewonnen!")
+if Scorespeler2 > Scorespeler1:
+    print("Speler 2 heeft gewonnen!")
+if Scorespeler1 == Scorespeler2:
+    print("gelijkspel")
